062a1dd73379c78d84d64a20d88870094528bf06
Etienne Dumai <etienne.dumai@gmail.com>
2024-10-02 16:40:14 +0200

    Ajout du fichier c du TD1

---
+++ TD/TD1/main.c
@@ -0,0 +1,15 @@
+/*
+Question 1
+Il y aura eu 4 processus quand le programme sera terminé.
+A : 1 B: 2 C : 4
+*/
+#include <stdio.h>
+#include <unistd.h>
+int main() {
+    //Question 2
+    //programme qui cherche un element dans un tableau de grande taille
+    //definissions des variables
+    int xTrouve = 0;
+    int tableau[1000000];
+    
+}
