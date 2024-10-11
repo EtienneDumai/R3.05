fdc821bcd46bf72ec7d6530589d495ac5aeada7a
Etienne Dumai <etienne.dumai@gmail.com>
2024-10-11 09:35:21 +0200

    TP2 fini

---
+++ TP/TP3/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+unsigned long int cpt = 0;
+void* compteur(void* arg)
+{
+    unsigned long int tmp;
+    for (int i = 0; i < 1000000; i++)
+    {
+        
+    }
+    
+}
+int main (void)
+{
+    return 0;
+}
