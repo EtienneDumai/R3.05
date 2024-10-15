ab1b6b46d377143b58193bf9021161a7b2509f32
Etienne Dumai <etienne.dumai@gmail.com>
2024-10-15 16:43:26 +0200

    TP3 fini

--- TD/TD3/main.c
+++ TD/TD3/main.c
@@ -2,38 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define TAILLE 1000000
-int tab[TAILLE];
-int somme = 0;
-int tube[2];
-
 int main(void)
 {
-    int x = pipe(tube);
-    pid_t pid = fork();
-    if (x == 1)
-    {
-        if (pid < 0)
-        {
-            printf("Erreur dans la creation du fork");
-        }
-        if (pid == 0)
-        {
-            printf("Je suis le processus fils\n");
-            for (int i = 0; i < TAILLE / 2; i++)
-            {
-                somme += tab[i];
-            }
-        }
-        else
-        {
-            printf("Je suis le processus pere\n");
-            for (int i = TAILLE / 2; i < TAILLE; i++)
-            {
-                somme += tab[i];
-            }
-        }
-    }
-
+    
     return 0;
 }
