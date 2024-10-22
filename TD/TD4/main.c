65169acd1707b0b8dfc7a4c5bdafbdfa8dd39fbb
Etienne Dumai <etienne.dumai@gmail.com>
2024-10-22 11:48:43 +0200

    Q1 du TD4 faite

---
+++ TD/TD4/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+void afficher (int sig) {
+    printf("ALors ca vient ?\n");
+}
+
+int main(void){
+    struct sigaction sig_relance, ancien;
+    char c;
+    sig_relance.sa_handler = afficher;
+    sig_relance.sa_flags = SA_RESTART;
+    sigemptyset(&sig_relance.sa_mask);
+    sigaction(SIGALRM, &sig_relance, &ancien);
+    alarm(15);
+    c=getchar();
+    alarm(0);
+    sigaction(SIGALRM, &ancien, NULL);
+    sleep(50);
+    exit(EXIT_SUCCESS);
+    return 0;
+}
